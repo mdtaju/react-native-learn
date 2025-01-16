01a8b7bbac7eeffc167d21724cf6c2f3e38bcbf4
mdtaju <tajumd512ctg@gmail.com>
2025-01-16 12:36:33 +0600

    env removed

--- env.ts
+++ env.ts
@@ -1,11 +1,14 @@
+// react native -
+// project = react-native-practice
+
 const appWriteConfig = {
-  endpoint: "https://cloud.appwrite.io/v1",
-  platform: "com.tajuuddin.aora",
-  projectId: "677915d00036ff478103",
-  databaseId: "67796c640036e07f1214",
-  userCollectionId: "67796fad002def40697b",
-  videoCollectionId: "677a1d75003b988ce235",
-  storageId: "677a2dc6003265795998",
+  endpoint: "",
+  platform: "",
+  projectId: "",
+  databaseId: "",
+  userCollectionId: "",
+  videoCollectionId: "",
+  storageId: "",
 };
 
 export default appWriteConfig;
